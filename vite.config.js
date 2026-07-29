2574c3a34a132ae2439ab162f0fe3121e40d0017
Granthali <granthalijadhav8@email.com>
2026-07-29 21:56:52 +0530

    Force fix base path to SCT_WD_3

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/quiz-game-app/',
+  base: '/SCT_WD_3/',
   plugins: [react()],
 })
